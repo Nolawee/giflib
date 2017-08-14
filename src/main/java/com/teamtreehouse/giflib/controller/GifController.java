9c20439582a91911674f192ef22c360a5c78a61d
Nolawee Mengist <nome9478@colorado.edu>
2017-08-14 10:59:48 -0600

    Data Through ThymeLeaf Templates

--- src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -1,6 +1,9 @@
 package com.teamtreehouse.giflib.controller;
 
+import com.teamtreehouse.giflib.model.Gif;
+import java.time.LocalDate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -9,5 +12,12 @@ public class GifController {
     @RequestMapping(value = "/")
     public String listGifs() {
         return "home";
+    }
+
+    @RequestMapping(value = "/gif")
+    public String gifDetails(ModelMap modelMap){
+        Gif gif = new Gif("compiler-bot", LocalDate.of(2015,2,13), "Nolawee",true);
+        modelMap.put("gif",gif);
+        return "gif-details";
     }
 }
