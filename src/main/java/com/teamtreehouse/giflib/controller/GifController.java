94164ae0137d6dc0af58fd0df6b9445f7a641ee2
Nolawee Mengist <nome9478@colorado.edu>
2017-08-14 16:54:16 -0600

    work in progress

--- src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -1,7 +1,10 @@
 package com.teamtreehouse.giflib.controller;
 
+import com.teamtreehouse.giflib.data.GifRepository;
 import com.teamtreehouse.giflib.model.Gif;
 import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +12,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GifController {
+
+    @Autowired
+    private GifRepository gifRepository;
+
     @RequestMapping(value = "/")
     public String listGifs() {
         return "home";
@@ -16,7 +23,7 @@ public class GifController {
 
     @RequestMapping(value = "/gif")
     public String gifDetails(ModelMap modelMap){
-        Gif gif = new Gif("android-explosion", LocalDate.of(2015,2,13), "Nolawee",true);
+        Gif gif = gifRepository.findByName("android-explosion");
         modelMap.put("gif",gif);
         return "gif-details";
     }
